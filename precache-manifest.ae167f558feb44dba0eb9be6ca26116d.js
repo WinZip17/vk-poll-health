652e1f63631a82f64437aeccfd534a6ffbecb0e4
WinZip <wydo@protonmail.com>
2020-02-18 20:33:35 +0300

    Updates

---
+++ precache-manifest.ae167f558feb44dba0eb9be6ca26116d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9d782ce45eb86329be55fac0f600c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e90dc3fee829efec224e",
+    "url": "/static/css/2.3649549b.chunk.css"
+  },
+  {
+    "revision": "962b21cea1936e29e65a",
+    "url": "/static/css/main.1fbd4a8a.chunk.css"
+  },
+  {
+    "revision": "e90dc3fee829efec224e",
+    "url": "/static/js/2.30131e6c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.30131e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962b21cea1936e29e65a",
+    "url": "/static/js/main.0918ea39.chunk.js"
+  },
+  {
+    "revision": "f0abff011e7277b2ad95",
+    "url": "/static/js/runtime-main.74744584.js"
+  }
+]);
